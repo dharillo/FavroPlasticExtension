a82217b7a1265a1a61074a3735a5e8e117887c05
David Harillo Sánchez <dhs.seck@gmail.com>
2019-06-14 01:02:42 +0200

    Initial commit

---
+++ FavroPlasticExtension/FavroExtension.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using log4net;
+
+namespace Codice.Client.IssueTracker.FavroExtension
+{
+    public class FavroExtension : IPlasticIssueTrackerExtension
+    {
+        internal static readonly ILog logger = LogManager.GetLogger("favroextension");
+
+        internal const string KEY_USER = "User";
+        internal const string KEY_PASSWORD = "Password";
+        internal const string KEY_ORGANIZATION = "Organization";
+
+        private const string EXTENSION_NAME = "Favro extension";
+        private IssueTrackerConfiguration configuration;
+
+        internal FavroExtension(IssueTrackerConfiguration configuration)
+        {
+            this.configuration = configuration;
+
+            logger.Info("Favro issue tracker is initialized");
+        }
+
+        #region IPlasticIssueTrackerExtension implementation
+        public string GetExtensionName()
+        {
+            return EXTENSION_NAME;
+        }
+
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TestConnection(IssueTrackerConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogCheckinResult(PlasticChangeset changeset, List<PlasticTask> tasks)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateLinkedTasksToChangeset(PlasticChangeset changeset, List<string> tasks)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlasticTask GetTaskForBranch(string fullBranchName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, PlasticTask> GetTasksForBranches(List<string> fullBranchNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenTaskExternally(string taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlasticTask> LoadTasks(List<string> taskIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlasticTask> GetPendingTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlasticTask> GetPendingTasks(string assignee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkTaskAsOpen(string taskId, string assignee)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ FavroPlasticExtension/FavroExtensionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using log4net;
+
+namespace Codice.Client.IssueTracker.FavroExtension
+{
+    public class FavroExtensionFactory: IPlasticIssueTrackerExtensionFactory
+    {
+        private const string ISSUE_TRACKER_NAME = "Favro Issue Tracker";
+        #region IPlasticIssueTrackerExtensionFactory implementation
+        public IssueTrackerConfiguration GetConfiguration(IssueTrackerConfiguration storedConfiguration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlasticIssueTrackerExtension GetIssueTrackerExtension(IssueTrackerConfiguration configuration)
+        {
+            return new FavroExtension(configuration);
+        }
+
+        public string GetIssueTrackerName()
+        {
+            return ISSUE_TRACKER_NAME;
+        }
+        #endregion
+    }
+}
