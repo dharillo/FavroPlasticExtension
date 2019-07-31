5dd3d8358724b202cc936f20a8f6fcaaccf91e52
David Harillo Sánchez <dhs.seck@gmail.com>
2019-08-01 00:03:49 +0200

    Fix license filenames and unsaved file license comment

--- FavroPlasticExtensionTests/Favro/API/ResponseTests.cs
+++ FavroPlasticExtensionTests/Favro/API/ResponseTests.cs
@@ -1,18 +1,18 @@
-﻿<one line to give the program's name and a brief idea of what it does.>
-    Copyright(C) <year>  <name of author>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.If not, see<https://www.gnu.org/licenses/>
+﻿//  Favro Plastic Extension
+//  Copyright(C) 2019  David Harillo Sánchez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published
+//  by the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Lesser General Public License for more details in the project root.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program. If not, see<https://www.gnu.org/licenses/>
 
 using FavroPlasticExtension.Favro;
 using FavroPlasticExtension.Favro.API;
