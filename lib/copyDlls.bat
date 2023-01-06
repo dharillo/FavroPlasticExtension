e2160d1ff210ac36e033a1c7204bc8a77193745c
Benjamín de la Fuente Ranea <yosoymin@gmail.com>
2023-01-06 11:31:44 +0100

    update to Plastic version 11.0.16.7696

--- lib/copyDlls.bat
+++ lib/copyDlls.bat
@@ -1,3 +1,3 @@
-copy "C:\Program Files\PlasticSCM5\client\issuetrackerinterface.dll" .
-copy "C:\Program Files\PlasticSCM5\client\log4net.dll" .
-copy "C:\Program Files\PlasticSCM5\client\utils.dll" .
+copy "C:\Program Files\PlasticSCM5\client\issuetrackerinterface.dll" %~dp0
+copy "C:\Program Files\PlasticSCM5\client\log4net.dll" %~dp0
+copy "C:\Program Files\PlasticSCM5\client\utils.dll" %~dp0
